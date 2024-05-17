f2da59b145a371a3f39f512d188b691f47de8e72
epcst-chua <chua.joseph@epcst.edu.ph>
2024-05-17 13:37:18 +0800

    caching

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register("/serviceworker.js");
+  navigator.serviceWorker.register("/sw.js");
 
   const urlsToCache = [
     "/index.html",
