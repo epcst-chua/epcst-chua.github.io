977d0488812f06d77480ea573da95e4c1160b749
epcst-chua <chua.joseph@epcst.edu.ph>
2024-05-17 13:42:46 +0800

    caching

--- sw.js
+++ sw.js
@@ -1,25 +1,21 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register("/sw.js");
+const urlsToCache = [
+  "/index.html",
+  "/images/icon-192.png", "/images/icon-512-maskable.png", "icon-512.png", "favicon.ico", "apple-touch-icon.png"
+];
 
-  const urlsToCache = [
-    "/index.html",
-    "/images/icon-192.png", "/images/icon-512-maskable.png", "icon-512.png", "favicon.ico", "apple-touch-icon.png"
-  ];
+// This code executes in its own worker or thread
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("pwa-assets").then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
 
-  // This code executes in its own worker or thread
-  self.addEventListener("install", event => {
-    event.waitUntil(
-      caches.open("pwa-assets").then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-    );
-  });
+self.addEventListener("activate", event => {
+  console.log("Service worker activated");
+});
 
-  self.addEventListener("activate", event => {
-    console.log("Service worker activated");
-  });
-
-  self.addEventListener("fetch", event => {
-    event.respondWith(caches.match(event.request));
- });
-}
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request));
+});
